80c437d42eba1cf7acee975a56b3b700df62b18d
Asad tanvir shami <asadworkemail@gmail.com>
2024-12-27 19:01:33 +0500

    push

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   // Set global prefix
   const globalPrefix = '/public/api/v1';
   app.setGlobalPrefix(globalPrefix);
-  
+
   // Enable CORS for all origins
-  app.enableCors();
+  app.enableCors({
+    origin:'https://www.algorimsoftware.io/',
+  });
 
   await app.listen(8080);
 }
